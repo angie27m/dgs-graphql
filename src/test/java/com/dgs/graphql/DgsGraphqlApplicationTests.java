66b4cc0beeba53c6dbbb63de60d84294a17232c5
Angie Manrique <angie27manrique@gmail.com>
2024-10-04 17:58:45 -0500

    Initial setup spring boot project with DGS Framework to work with GraphQL

---
+++ src/test/java/com/dgs/graphql/DgsGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgs.graphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DgsGraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
